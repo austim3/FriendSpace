12e19040d0699445d34e69f6ebc32bf2e42e8f32
austim3 <947818826@qq.com>
2018-07-18 11:13:33 +0800

    1

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ import ChangePersonInfoScreen from './Screen/ChangePersonInfoScreen';
 import HomeScreen from './Screen/HomeScreen';
 import CreateMessageScreen from './Screen/CreateMessageScreen';
 import CreateUserScreen from './Screen/CreateUserScreen';
+import AddFriendScreen from './Screen/AddFriendScreen';
+import FriendScreen from './Screen/FriendScreen';
+import PerssonInfoScreen from './Screen/PerssonInfoScreen';
+import LoginScreen from './Screen/LoginScreen';
+import EditPasswordScreen from './Screen/EditPasswordScreen';
 import AScreen from './Screen/AScreen';
 import BScreen from './Screen/BScreen';
 
@@ -39,7 +44,11 @@ class App extends Component {
           <Route path={'/CreateUserScreen'} component={CreateUserScreen} />
           <Route path={'/CreateMessageScreen'} component={CreateMessageScreen} />
           <Route path={'/HomeScreen'} component={HomeScreen} />
-          <Route path={'/HomeScreen'} component={HomeScreen} />
+          <Route path={'/FriendScreen'} component={FriendScreen} />
+          <Route path={'/AddFriendScreen'} component={AddFriendScreen} />
+          <Route path={'/LoginScreen'} component={LoginScreen} />
+          <Route path={'/EditPasswordScreen'} component={EditPasswordScreen} />
+          <Route path={'/PerssonInfoScreen'} component={PerssonInfoScreen} />
         </div>
       </DocumentTitle>
     );
